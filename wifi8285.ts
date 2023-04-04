916133e8aa259e282cf78c719cba5236ddb90a23
Ralf Krause <ralf.krause@gmail.com>
2023-04-04 16:51:59 +0200

    change icon

--- wifi8285.ts
+++ wifi8285.ts
@@ -1,7 +1,7 @@
 /**
  * Functions for Wifi8285 module.
  */
-//% weight=10 color=#606060 icon="\uf1b3" block="Wifi8285"
+//% weight=10 color=#606060 icon="\uf1eb" block="Wifi8285"
 
 namespace Wifi8285 {
 
